850a7d1bcd70a8959e9614cf61eb3515f52a34b0
Kerolos Zaki <ker0olos@pm.me>
2022-09-10 10:04:10 +0200

    action.yml: fix entrypoint file path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph /usr/lib/libmediagraph.so
+cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so /usr/lib/libmediagraph.so
 cp /mediapipe/mediapipe/lib/mediagraph/mediagraph.h /usr/include/mediagraph.h
 
-cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph /usr/lib/x86_64-linux-gnu/libmediagraph.so
+cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so /usr/lib/x86_64-linux-gnu/libmediagraph.so
 cp /mediapipe/mediapipe/lib/mediagraph/mediagraph.h /usr/include/x86_64-linux-gnu/mediagraph.h
