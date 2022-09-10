2a8bbe1e415099cc44f084e3271fe09116d90a70
Kerolos Zaki <ker0olos@pm.me>
2022-09-10 13:22:13 +0200

    Added a github action to build mediagraph (#1)

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "Installing library..."
+LIB_FILE="/mediapipe/libmediagraph.so"
+cp "$LIB_FILE" /usr/lib/libmediagraph.so || exit 1
+cp "$LIB_FILE" /usr/lib/x86_64-linux-gnu/libmediagraph.so || exit 1
+
+echo "Installing header file..."
+HEADER_FILE="/mediapipe/mediagraph.h"
+cp "$HEADER_FILE" /usr/include/mediagraph.h || exit 1
+cp "$HEADER_FILE" /usr/include/x86_64-linux-gnu/mediagraph.h || exit 1
