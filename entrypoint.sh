01231c97b296309e36f7df0280f4b8bee9611c0b
Kerolos Zaki <ker0olos@pm.me>
2022-09-10 10:34:52 +0200

    fail ci if copying files fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
-echo "Installing libraries..."
-cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so /usr/lib/libmediagraph.so || exit 1
-cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so /usr/lib/x86_64-linux-gnu/libmediagraph.so || exit 1
+echo "Installing library..."
+LIB_FILE="/mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so"
+if [ -f "$LIB_FILE" ]; then
+  cp "$LIB_FILE" "/usr/lib/libmediagraph.so"
+  cp "$LIB_FILE" "/usr/lib/x86_64-linux-gnu/libmediagraph.so"
+else 
+    echo "Could not find library file"
+    exit 1
+fi
 
-echo "Installing header files..."
-cp /mediapipe/mediapipe/lib/mediagraph/mediagraph.h /usr/include/mediagraph.h || exit 1
-cp /mediapipe/mediapipe/lib/mediagraph/mediagraph.h /usr/include/x86_64-linux-gnu/mediagraph.h || exit 1
+echo "Installing header file..."
+HEADER_FILE="/mediapipe/mediapipe/lib/mediagraph/mediagraph.h"
+if [ -f "$HEADER_FILE" ]; then
+  cp "$HEADER_FILE" "/usr/include/mediagraph.h"
+  cp "$HEADER_FILE" "/usr/include/x86_64-linux-gnu/mediagraph.h"
+else 
+    echo "Could not find header file"
+    exit 1
+fi
+
