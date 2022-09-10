7b5888fbbf436c973f7bafd02a00aab13a6d8322
Kerolos Zaki <ker0olos@pm.me>
2022-09-10 12:21:55 +0200

    fixed header path

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ cp "$LIB_FILE" /usr/lib/libmediagraph.so || exit 1
 cp "$LIB_FILE" /usr/lib/x86_64-linux-gnu/libmediagraph.so || exit 1
 
 echo "Installing header file..."
-HEADER_FILE=" /mediapipe/mediagraph.h"
+HEADER_FILE="/mediapipe/mediagraph.h"
 cp "$HEADER_FILE" /usr/include/mediagraph.h || exit 1
 cp "$HEADER_FILE" /usr/include/x86_64-linux-gnu/mediagraph.h || exit 1
