db94c11d649f550d9c8af4d7c2f8cc2b955a7c36
Kerolos Zaki <ker0olos@pm.me>
2022-09-10 10:25:09 +0200

    fail ci if copying files fails

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so /usr/lib/libmediagraph.so
-cp /mediapipe/mediapipe/lib/mediagraph/mediagraph.h /usr/include/mediagraph.h
+echo "Installing libraries..."
+cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so /usr/lib/libmediagraph.so || exit 1
+cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so /usr/lib/x86_64-linux-gnu/libmediagraph.so || exit 1
 
-cp /mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so /usr/lib/x86_64-linux-gnu/libmediagraph.so
-cp /mediapipe/mediapipe/lib/mediagraph/mediagraph.h /usr/include/x86_64-linux-gnu/mediagraph.h
+echo "Installing header files..."
+cp /mediapipe/mediapipe/lib/mediagraph/mediagraph.h /usr/include/mediagraph.h || exit 1
+cp /mediapipe/mediapipe/lib/mediagraph/mediagraph.h /usr/include/x86_64-linux-gnu/mediagraph.h || exit 1
