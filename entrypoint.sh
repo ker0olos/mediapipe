08ba848fb64da41f94441d1fd439464451333d6d
Kerolos Zaki <ker0olos@pm.me>
2022-09-10 10:44:50 +0200

    fix_17... github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,11 @@
 #!/bin/sh
 
 echo "Installing library..."
-LIB_FILE="/mediapipe/bazel-bin/mediapipe/lib/mediagraph/libmediagraph.so"
-if [ -f "$LIB_FILE" ]; then
-  cp "$LIB_FILE" "/usr/lib/libmediagraph.so"
-  cp "$LIB_FILE" "/usr/lib/x86_64-linux-gnu/libmediagraph.so"
-else 
-    echo "Could not find library file"
-    exit 1
-fi
+LIB_FILE="/mediapipe/libmediagraph.so"
+cp "$LIB_FILE" /usr/lib/libmediagraph.so || exit 1
+cp "$LIB_FILE" /usr/lib/x86_64-linux-gnu/libmediagraph.so || exit 1
 
 echo "Installing header file..."
-HEADER_FILE="/mediapipe/mediapipe/lib/mediagraph/mediagraph.h"
-if [ -f "$HEADER_FILE" ]; then
-  cp "$HEADER_FILE" "/usr/include/mediagraph.h"
-  cp "$HEADER_FILE" "/usr/include/x86_64-linux-gnu/mediagraph.h"
-else 
-    echo "Could not find header file"
-    exit 1
-fi
-
+HEADER_FILE=" /mediapipe/mediagraph.h"
+cp "$HEADER_FILE" /usr/include/mediagraph.h || exit 1
+cp "$HEADER_FILE" /usr/include/x86_64-linux-gnu/mediagraph.h || exit 1
