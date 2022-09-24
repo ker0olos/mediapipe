0a9ced49cfc713a828dc8ff1245c1e1d79fde928
Kerolos Zaki <ker0olos@pm.me>
2022-09-24 23:43:57 +0200

    collect and return a face mesh bbox

--- mediapipe/lib/mediagraph/mediagraph.cc
+++ mediapipe/lib/mediagraph/mediagraph.cc
@@ -38,6 +38,170 @@ struct Landmark {
   float presence;
 };
 
+struct FaceBox {
+  float xmin;
+  float xmax;
+  float ymin;
+  float ymax;
+  float zmin;
+  float zmax;
+};
+
+//
+
+class FaceMeshGraph {
+  public:
+    FaceMeshGraph();
+    bool process(const cv::Mat *input, Landmark output[478], FaceBox &face_box);
+    ~FaceMeshGraph();
+  private:
+    std::unique_ptr<mediapipe::OutputStreamPoller> poller;
+    std::unique_ptr<mediapipe::CalculatorGraph> graph;
+};
+
+FaceMeshGraph::FaceMeshGraph() {
+  const char* output_node = "multi_face_landmarks";
+  const char* graph_config = R""""(
+  input_stream: "input_video"
+  output_stream: "multi_face_landmarks"
+
+  node {
+    calculator: "FlowLimiterCalculator"
+    input_stream: "input_video"
+    input_stream: "FINISHED:multi_face_landmarks"
+    input_stream_info: {
+      tag_index: "FINISHED"
+      back_edge: true
+    }
+    output_stream: "throttled_input_video"
+  }
+
+  node {
+    calculator: "ConstantSidePacketCalculator"
+    output_side_packet: "PACKET:0:num_faces"
+    output_side_packet: "PACKET:1:with_attention"
+    node_options: {
+      [type.googleapis.com/mediapipe.ConstantSidePacketCalculatorOptions]: {
+        packet { int_value: 1 }
+        packet { bool_value: true }
+      }
+    }
+  }
+
+  node {
+    calculator: "FaceLandmarkFrontCpu"
+    input_stream: "IMAGE:throttled_input_video"
+    input_side_packet: "NUM_FACES:num_faces"
+    input_side_packet: "WITH_ATTENTION:with_attention"
+    output_stream: "LANDMARKS:multi_face_landmarks"
+    output_stream: "ROIS_FROM_LANDMARKS:face_rects_from_landmarks"
+    output_stream: "DETECTIONS:face_detections"
+    output_stream: "ROIS_FROM_DETECTIONS:face_rects_from_detections"
+  }
+)"""";
+  
+  mediapipe::CalculatorGraphConfig config = mediapipe::ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig>(
+      graph_config);
+
+  graph = std::make_unique<mediapipe::CalculatorGraph>();
+
+  absl::Status status = graph->Initialize(config);
+
+  if (!status.ok()) {
+    std::cerr << "Graph Init" << status.message() << std::endl;
+    throw std::runtime_error("Failed to create the graph");
+  }
+
+  mediapipe::StatusOrPoller sop = graph->AddOutputStreamPoller(output_node);
+
+   if (!sop.ok()) {
+     std::cerr << "Failed to add poller to graph: [" << sop.status().message() << "]" << std::endl;
+     throw std::runtime_error("Failed to add poller to graph");
+   }
+
+  assert(sop.ok());
+
+  poller = std::make_unique<mediapipe::OutputStreamPoller>(std::move(sop.value()));
+
+  graph->StartRun({});
+}
+
+FaceMeshGraph::~FaceMeshGraph() {
+  graph->CloseInputStream(kInputStream);
+  graph->WaitUntilDone();
+}
+
+bool FaceMeshGraph::process(const cv::Mat *input, Landmark output[478], FaceBox &face_box) {
+  auto input_frame = absl::make_unique<mediapipe::ImageFrame>(
+      mediapipe::ImageFormat::SRGB, input->cols, input->rows,
+      mediapipe::ImageFrame::kDefaultAlignmentBoundary);
+
+  cv::Mat input_frame_mat = mediapipe::formats::MatView(input_frame.get());
+  input->copyTo(input_frame_mat);
+
+  size_t frame_timestamp_us =
+      (double)cv::getTickCount() / (double)cv::getTickFrequency() * 1e6;
+
+  absl::Status run_status = graph->AddPacketToInputStream(
+      kInputStream, mediapipe::Adopt(input_frame.release())
+                        .At(mediapipe::Timestamp(frame_timestamp_us)));
+
+  if (!run_status.ok()) {
+    std::cerr << "Add Packet error: [" << run_status.message() << "]" << std::endl;
+    return false;
+  }
+
+  mediapipe::Packet packet;
+  if (poller->QueueSize() > 0) {
+    if (poller->Next(&packet)) {
+      auto& faces = packet.Get<std::vector<mediapipe::NormalizedLandmarkList>>();
+
+      if (faces.size() > 0) {
+          const mediapipe::NormalizedLandmarkList &face = faces.at(0);
+          
+          for (int idx = 0; idx < face.landmark_size(); ++idx) {
+            const mediapipe::NormalizedLandmark& landmark = face.landmark(idx);
+
+            output[idx] = {
+              .x = landmark.x(),
+              .y = landmark.y(),
+              .z = landmark.z(),
+              .visibility = 0,
+              .presence = 0,
+            };
+
+            if (idx == 0) {
+              face_box = {
+                .xmin = landmark.x(),
+                .xmax = landmark.x(),
+                .ymin = landmark.y(),
+                .ymax = landmark.y(),
+                .zmin = landmark.z(),
+                .zmax = landmark.z(),
+              };
+            } else {
+              face_box = {
+                .xmin = std::min(landmark.x(), face_box.xmin),
+                .xmax = std::max(landmark.x(), face_box.xmax),
+                .ymin = std::min(landmark.y(), face_box.ymin),
+                .ymax = std::max(landmark.y(), face_box.ymax),
+                .zmin = std::min(landmark.z(), face_box.zmin),
+                .zmax = std::max(landmark.z(), face_box.zmax),
+              };
+            }
+          }
+      }
+    } else {
+        std::cerr << "No packet from poller" << std::endl;
+        return false;
+    }
+  }
+
+  return true;
+}
+
+//
+
 class PoseGraph {
   public: 
     PoseGraph();
@@ -92,6 +256,7 @@ PoseGraph::PoseGraph() {
   graph = std::make_unique<mediapipe::CalculatorGraph>();
   
   absl::Status status = graph->Initialize(config);
+
   if (!status.ok()) {
     std::cerr << "Graph Init" << status.message() << std::endl;
     throw std::runtime_error("Failed to create the graph");
@@ -155,136 +320,6 @@ bool PoseGraph::process(const cv::Mat *input, Landmark output[33]) {
     } else {
       std::cerr << "No packet from poller" << std::endl; 
       return false;
-    }
-  }
-
-  return true;
-}
-
-class FaceMeshGraph {
-  public:
-    FaceMeshGraph();
-    bool process(const cv::Mat *input, Landmark output[478]);
-    ~FaceMeshGraph();
-  private:
-    std::unique_ptr<mediapipe::OutputStreamPoller> poller;
-    std::unique_ptr<mediapipe::CalculatorGraph> graph;
-};
-
-FaceMeshGraph::FaceMeshGraph() {
-  const char* output_node = "multi_face_landmarks";
-  const char* graph_config = R""""(
-  input_stream: "input_video"
-  output_stream: "multi_face_landmarks"
-
-  node {
-    calculator: "FlowLimiterCalculator"
-    input_stream: "input_video"
-    input_stream: "FINISHED:multi_face_landmarks"
-    input_stream_info: {
-      tag_index: "FINISHED"
-      back_edge: true
-    }
-    output_stream: "throttled_input_video"
-  }
-
-  node {
-    calculator: "ConstantSidePacketCalculator"
-    output_side_packet: "PACKET:0:num_faces"
-    output_side_packet: "PACKET:1:with_attention"
-    node_options: {
-      [type.googleapis.com/mediapipe.ConstantSidePacketCalculatorOptions]: {
-        packet { int_value: 1 }
-        packet { bool_value: true }
-      }
-    }
-  }
-
-  node {
-    calculator: "FaceLandmarkFrontCpu"
-    input_stream: "IMAGE:throttled_input_video"
-    input_side_packet: "NUM_FACES:num_faces"
-    input_side_packet: "WITH_ATTENTION:with_attention"
-    output_stream: "LANDMARKS:multi_face_landmarks"
-    output_stream: "ROIS_FROM_LANDMARKS:face_rects_from_landmarks"
-    output_stream: "DETECTIONS:face_detections"
-    output_stream: "ROIS_FROM_DETECTIONS:face_rects_from_detections"
-  }
-)"""";
-  
-  mediapipe::CalculatorGraphConfig config = mediapipe::ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig>(
-      graph_config);
-
-  graph = std::make_unique<mediapipe::CalculatorGraph>();
-
-  absl::Status status = graph->Initialize(config);
-  if (!status.ok()) {
-    std::cerr << "Graph Init" << status.message() << std::endl;
-    throw std::runtime_error("Failed to create the graph");
-  }
-
-  mediapipe::StatusOrPoller sop = graph->AddOutputStreamPoller(output_node);
-
-   if (!sop.ok()) {
-     std::cerr << "Failed to add poller to graph: [" << sop.status().message() << "]" << std::endl;
-     throw std::runtime_error("Failed to add poller to graph");
-   }
-
-  assert(sop.ok());
-
-  poller = std::make_unique<mediapipe::OutputStreamPoller>(std::move(sop.value()));
-
-  graph->StartRun({});
-}
-
-FaceMeshGraph::~FaceMeshGraph() {
-  graph->CloseInputStream(kInputStream);
-  graph->WaitUntilDone();
-}
-
-bool FaceMeshGraph::process(const cv::Mat *input, Landmark output[478]) {
-  auto input_frame = absl::make_unique<mediapipe::ImageFrame>(
-      mediapipe::ImageFormat::SRGB, input->cols, input->rows,
-      mediapipe::ImageFrame::kDefaultAlignmentBoundary);
-
-  cv::Mat input_frame_mat = mediapipe::formats::MatView(input_frame.get());
-  input->copyTo(input_frame_mat);
-
-  size_t frame_timestamp_us =
-      (double)cv::getTickCount() / (double)cv::getTickFrequency() * 1e6;
-
-  absl::Status run_status = graph->AddPacketToInputStream(
-      kInputStream, mediapipe::Adopt(input_frame.release())
-                        .At(mediapipe::Timestamp(frame_timestamp_us)));
-
-  if (!run_status.ok()) {
-    std::cerr << "Add Packet error: [" << run_status.message() << "]" << std::endl;
-    return false;
-  }
-
-  mediapipe::Packet packet;
-  if (poller->QueueSize() > 0) {
-    if (poller->Next(&packet)) {
-      auto& faces = packet.Get<std::vector<mediapipe::NormalizedLandmarkList>>();
-
-      if (faces.size() > 0) {
-          const mediapipe::NormalizedLandmarkList &face = faces.at(0);
-          
-          for (int idx = 0; idx < face.landmark_size(); ++idx) {
-            const mediapipe::NormalizedLandmark& landmark = face.landmark(idx);
-
-            output[idx] = {
-              .x = landmark.x(),
-              .y = landmark.y(),
-              .z = landmark.z(),
-              .visibility = 0,
-              .presence = 0,
-            };
-          }
-      }
-    } else {
-        std::cerr << "No packet from poller" << std::endl;
-        return false;
     }
   }
 
--- mediapipe/lib/mediagraph/mediagraph.h
+++ mediapipe/lib/mediagraph/mediagraph.h
@@ -9,10 +9,19 @@ struct Landmark {
   float visibility;
   float presence;
 };
+struct FaceBox;
+struct FaceBox {
+  float xmin;
+  float xmax;
+  float ymin;
+  float ymax;
+  float zmin;
+  float zmax;
+};
 class FaceMeshGraph {
   public:
     FaceMeshGraph();
-    bool process(const cv::Mat *input, Landmark output[478]);
+    bool process(const cv::Mat *input, Landmark output[478], FaceBox &face_box);
     ~FaceMeshGraph();
   private:
     void* poller;
